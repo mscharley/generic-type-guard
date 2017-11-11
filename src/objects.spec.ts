8e5bc508c46f38f12b0335d89eb05d5fc7a4ec77
Matthew Scharley <matt.scharley@gmail.com>
2017-11-11 19:40:03 +1100

    Define an isInstance check

--- src/objects.spec.ts
+++ src/objects.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { slow, suite, test, timeout } from "mocha-typescript";
+import { DummyClass } from "./dummy_class.spec";
 import * as o from "./objects";
 import * as p from "./primitives";
 
@@ -13,5 +14,15 @@ export class ObjectsSpec {
     expect(hasFooString({ foo: "bar" })).to.equal(true);
     expect(hasFooString({})).to.equal(false);
     expect(hasFooString({ bar: "foo" })).to.equal(false);
+  }
+
+  @test public instance() {
+    const isDummyClass = o.isInstance(DummyClass);
+    expect(isDummyClass(new DummyClass())).to.equal(true);
+    // Basic objects should not work.
+    expect(isDummyClass({})).to.equal(false);
+    // Objects that are structurally similar should not work.
+    expect(isDummyClass({ foo: "bar" })).to.equal(false);
+    expect(isDummyClass("foo")).to.equal(false);
   }
 }
--- src/objects.ts
+++ src/objects.ts
@@ -6,3 +6,9 @@ import { PartialTypeGuard, TypeGuard } from "./guards";
 export const hasProperty =
   <K extends string, V>(property: K, value: TypeGuard<V>): PartialTypeGuard<{}, { [prop in K]: V }> =>
   (o): o is { [prop in K]: V } => o.hasOwnProperty(property) && value((o as { [prop: string]: any })[property]);
+
+/**
+ * Validates that a given object is an instance of a class.
+ */
+export const isInstance = <T extends {}>(klass: { new(...args: any[]): T }): TypeGuard<T> =>
+  (o): o is T => o instanceof klass;
