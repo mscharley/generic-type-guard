852dd82bb71d61afe5e25e0eee493fa64afbaeed
Matthew Scharley <matt.scharley@gmail.com>
2017-11-11 21:39:49 +1100

    Documentation!

---
+++ src/examples.spec.ts
@@ -0,0 +1,21 @@
+import * as tg from "./index";
+
+export interface TestInterface {
+  str: string;
+  num: number;
+}
+
+/**
+ * Simple way to test an interface.
+ */
+export const isTestInterface: tg.TypeGuard<TestInterface> = (o: any): o is TestInterface =>
+  tg.isObject(o) && tg.hasProperty("str", tg.isString)(o) && tg.hasProperty("num", tg.isNumber)(o);
+
+/**
+ * More type-safe way to test an interface.
+ *
+ * You can then combine this PartialTypeGuard with isObject to get a full typeguard
+ * from `any`.
+ */
+export const isTypeSafeTestInterface: tg.PartialTypeGuard<{}, TestInterface> =
+  tg.isIntersection(tg.hasProperty("str", tg.isString), tg.hasProperty("num", tg.isNumber));
