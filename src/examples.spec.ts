b05ff976b3408590c224e7f4574c13a61d90ddf3
Matthew Scharley <matt.scharley@gmail.com>
2017-11-11 21:45:18 +1100

    More documentation that slipped through

--- src/examples.spec.ts
+++ src/examples.spec.ts
@@ -15,7 +15,8 @@ export const isTestInterface: tg.TypeGuard<TestInterface> = (o: any): o is TestI
  * More type-safe way to test an interface.
  *
  * You can then combine this PartialTypeGuard with isObject to get a full typeguard
- * from `any`.
+ * from `any`. If you add properties to TestInterface or change types then this form will
+ * trigger a type error as the interfaces won't match up structurally.
  */
 export const isTypeSafeTestInterface: tg.PartialTypeGuard<{}, TestInterface> =
   tg.isIntersection(tg.hasProperty("str", tg.isString), tg.hasProperty("num", tg.isNumber));
