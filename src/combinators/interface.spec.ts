6aea989f4fbd122b1af8356af1de300ed70061ac
Matthew Scharley <matt.scharley@gmail.com>
2017-11-13 22:13:45 +1100

    Ensure that classes work properly with the property checks

--- src/combinators/interface.spec.ts
+++ src/combinators/interface.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { slow, suite, test, timeout } from "mocha-typescript";
+import { FullDummyClass, LargeInterface } from "../dummy_classes.spec";
 import { TypeGuard } from "../guards";
 import * as p from "../primitives";
 import { IsInterface } from "./interface";
@@ -17,7 +18,7 @@ export interface SimpleInterface {
 @suite(timeout(3000), slow(5))
 export class InterfaceSpec {
   // This class is a wrapper around isUnion, most of the real tests happen there.
-  @test public unionStringNumber() {
+  @test public interface() {
     const isSimpleInterface: TypeGuard<SimpleInterface> =
       new IsInterface()
         .withProperty("str", p.isString)
@@ -27,6 +28,16 @@ export class InterfaceSpec {
     expect(isSimpleInterface({ str: "foo", num: 10, b: false, n: null })).to.equal(true);
     expect(isSimpleInterface({ str: "foo", num: "foo", b: false, n: null })).to.equal(false);
     expect(isSimpleInterface(10)).to.equal(false);
+  }
+
+  @test public classImplementsInterface() {
+    const isLargeInterface: TypeGuard<LargeInterface> =
+      new IsInterface()
+        .withProperty("foo", p.isString)
+        .withProperty("bar", p.isNumber)
+        .withProperty("active", p.isBoolean)
+        .get();
+    expect(isLargeInterface(new FullDummyClass())).to.equal(true);
   }
 
   @test public emptyInterface() {
---
+++ src/dummy_classes.spec.ts
@@ -0,0 +1,22 @@
+// tslint:disable:max-classes-per-file
+
+export interface LargeInterface {
+  foo: string;
+  bar: number;
+  active: boolean;
+}
+
+/**
+ * Dummy class definition which can be used by the object tests.
+ */
+export class DummyClass {
+  public foo: string = "foo";
+}
+
+/**
+ * Dummy class that implements an interface with a mixture of own properties and base properties.
+ */
+export class FullDummyClass extends DummyClass implements LargeInterface {
+  public bar: number = 10;
+  public active: boolean = true;
+}
