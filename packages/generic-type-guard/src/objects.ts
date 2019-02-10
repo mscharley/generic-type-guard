e712bd5ad635429af7c407ed68fa790fcefe18a1
Matthew Scharley <matt@scharley.me>
2019-02-10 11:33:45 +1100

    chore: fix tslint

--- packages/generic-type-guard/src/objects.ts
+++ packages/generic-type-guard/src/objects.ts
@@ -73,5 +73,5 @@ export const hasNumericIndexSignature =
 /**
  * Validates that a given object is an instance of a class.
  */
-export const isInstance = <T extends {}>(klass: { new(...args: any[]): T }): TypeGuard<T> =>
+export const isInstance = <T extends {}>(klass: new(...args: any[]) => T): TypeGuard<T> =>
   (o): o is T => o instanceof klass;
