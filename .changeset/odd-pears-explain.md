6566b35fa6d4fc82a9cfd74050176de083105d53
Matthew Scharley <matt@scharley.me>
2024-10-07 18:54:12 +1100

    chore: migrate license to MPL-2.0

---
+++ .changeset/odd-pears-explain.md
@@ -0,0 +1,5 @@
+---
+'generic-type-guard': patch
+---
+
+Updated license to MPL-2.0 from MIT
--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import { configs } from "@mscharley/eslint-config";
+import { configs, withStyles } from "@mscharley/eslint-config";
+import notice from 'eslint-plugin-notice';
 
 export default [
 	...configs.recommended,
 	...configs.node,
+	...withStyles(),
 	{
 		ignores: [
 			'node_modules',
@@ -12,5 +14,24 @@ export default [
 			'eslint.config.js',
 			'generic-type-guard.*',
 		],
+	},
+	{
+		plugins: { notice },
+		files: ['src/**/*.{ts,js,tsx,jsx}'],
+		rules: {
+			'notice/notice': [
+				'error',
+				{
+					template:
+						'/**\n'
+						+ ' * This Source Code Form is subject to the terms of the Mozilla Public\n'
+						+ ' * License, v. 2.0. If a copy of the MPL was not distributed with this\n'
+						+ ' * file, You can obtain one at http://mozilla.org/MPL/2.0/.\n'
+						+ ' */\n\n',
+					// The package entrypoint needs some extra grace since the first comment must be the @packageDocumentation.
+					chars: 5000,
+				},
+			],
+		},
 	},
 ];
